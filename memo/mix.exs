dff79280cb7eaa8d8bb76d3d78c501dabc7a5c5a
Bob Stockdale <bstockdale@zappos.com>
2020-09-01 11:40:56 -0400

    add initial buttons

--- memo/mix.exs
+++ memo/mix.exs
@@ -46,7 +46,8 @@ defmodule Memo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:cowboy, "< 2.8.0", override: true}
     ]
   end
 
