ed251859171d0c97749eec40574a5af8f2d65279
alishahrokhi <ashahrokhi@projectronin.com>
2020-09-01 10:27:21 -0700

    Add Memo.Eraser

---
+++ memo/lib/memo/eraser.ex
@@ -0,0 +1,18 @@
+defmodule Memo.Eraser do
+
+  def new(text, steps) do
+    %{mask_text: text, plan: plan(text, steps)}
+  end
+
+  defp plan(text, steps) do
+    1..String.length(text)
+    |> Enum.shuffle
+    |> Enum.chunk_every(calculate_chuck_size(text,steps))
+  end
+
+  defp calculate_chuck_size(text,steps) do
+    String.length(text)
+    |> Kernel./(steps)
+    |> ceil
+  end
+end
